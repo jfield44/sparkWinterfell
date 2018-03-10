1200934511e13867c3c485e41eab61306c1fd937
Ryan Weddle <weddle@gmail.com>
2018-03-10 07:35:16 -0800

    added dependencies to begin including Spark SDK

--- app/src/main/java/com/ryanweddle/winterfell/SparkModel.java
+++ app/src/main/java/com/ryanweddle/winterfell/SparkModel.java
@@ -1,11 +1,18 @@
 package com.ryanweddle.winterfell;
 
+import com.ciscospark.androidsdk.Spark;
+import com.ciscospark.androidsdk.auth.Authenticator;
+
 /**
  * Created by ryweddle on 3/10/18.
  */
 
 class SparkModel {
     private static final SparkModel ourInstance = new SparkModel();
+
+    private Spark mSpark;
+    private Authenticator mAuthenticator;
+
 
     static SparkModel getInstance() {
         return ourInstance;
