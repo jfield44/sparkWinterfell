c7cd7f782efc3f738e730db11efc984aa4d2b143
Ryan Weddle <weddle@gmail.com>
2018-03-11 10:43:51 -0700

    Create DevConstants.java

---
+++ app/src/main/java/com/ryanweddle/winterfell/DevConstants.java
@@ -0,0 +1,11 @@
+package com.ryanweddle.winterfell;
+
+/**
+ * Created by ryweddle on 3/11/18.
+ */
+
+public class DevConstants {
+
+    public static final String EXAMPLE_JWT = "SAMPLE_JWT_TO_AUTH";
+    public static final String EXAMPLE_SPARKID = "DEFAULT_SPARK_USER_TO_DIAL";
+}
